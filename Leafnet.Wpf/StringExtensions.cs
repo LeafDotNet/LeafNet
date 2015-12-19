02aefb343f0fe459278b6a5b7ca9898d58793af0
georgebearden <georgebearden@gmail.com>
2015-12-18 23:17:50 -0500

    Full page leaflet rendering is now working from local web files.

---
+++ Leafnet.Wpf/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Leafnet.Wpf
+{
+  public static class StringExtensions
+  {
+    public static string TrimStart(this string s, string trim)
+    {
+      return s.TrimStart(trim.ToCharArray());
+    }
+  }
+}
