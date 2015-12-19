f80e5a1bce7f25bcfcb273295b9def57f0309cc6
georgebearden <georgebearden@gmail.com>
2015-12-18 19:17:28 -0800

    temp commit.

--- Leafnet.Wpf/MainWindow.xaml.cs
+++ Leafnet.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using CefSharp;
+using CefSharp.Wpf;
 
 namespace Leafnet.Wpf
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
+  public partial class MainWindow
   {
     public MainWindow()
     {
       InitializeComponent();
+
+      var resourceFactory = browser.ResourceHandlerFactory;
+
+      resourceFactory.GetResourceHandler(browser.WebBrowser, browser.GetBrowser(), browser.GetMainFrame(), null);
     }
   }
 }
