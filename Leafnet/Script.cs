2d38be6e00fe854dbc0f6183ad6e8ba2185e25c0
georgebearden <georgebearden@gmail.com>
2015-12-20 13:53:38 -0500

    Work on the C# to JS framework/functions.

---
+++ Leafnet/Script.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace Leafnet
+{
+  public static class Script
+  {
+    static ChromiumWebBrowser webBrowser = null;
+
+    public static void Initialize(ChromiumWebBrowser browser)
+    {
+      webBrowser = browser;
+    }
+
+    public static void ExecuteAsync(string script)
+    {
+      if (webBrowser == null)
+        throw new NullReferenceException("webBrowser");
+
+      webBrowser.ExecuteScriptAsync(script);
+    }
+
+    public static async Task<T> EvaluateAsync<T>(string script)
+    {
+      if (webBrowser == null)
+        throw new NullReferenceException("webBrowser");
+
+      var response = await webBrowser.EvaluateScriptAsync(script);
+      if (response.Result == null)
+        return default(T);
+
+      return (T) response.Result;
+    }
+  }
+}
