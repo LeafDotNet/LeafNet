1865e8e73818f5c1f49bc7b13b47c0a684ddb564
georgebearden <georgebearden@gmail.com>
2015-12-19 21:11:27 -0500

    Work on dynamic map loading from C#.

--- Leafnet.Wpf/MainWindow.xaml.cs
+++ Leafnet.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using CefSharp;
-using CefSharp.Wpf;
+﻿using System.Threading.Tasks;
+using CefSharp;
+using System;
 
 namespace Leafnet.Wpf
 {
@@ -10,6 +11,23 @@ namespace Leafnet.Wpf
       InitializeComponent();
 
       browser.Address = "custom://web/index.html";
+      browser.IsBrowserInitializedChanged += OnBrowserInitializedChanged;
+    }
+
+    private void OnBrowserInitializedChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+    {
+      if (browser.IsBrowserInitialized)
+      {
+        LoadMap();
+        //browser.ShowDevTools();
+      }
+    }
+
+    public async void LoadMap()
+    {
+      var s2 = "alert(document.body.style.backgroundColor)";
+      //var script = "L.map('map').setView([51.505, -0.09], 13);";
+      browser.ExecuteScriptAsync(s2);
     }
   }
 }
