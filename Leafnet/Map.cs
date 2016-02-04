e2ff28dbc9b548f87439fe5d544ecd6ce7e02a75
Mike Howard <mhowardx@gmail.com>
2016-02-04 00:03:36 -0500

    Experimenting with different ways of debugging.

--- Leafnet/Map.cs
+++ Leafnet/Map.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 using CefSharp;
 using Newtonsoft.Json;
 
@@ -17,8 +21,40 @@ namespace Leafnet
       var js = $"{JsName}.removeLayer({layer.JsName});";
       await Evaluate( js );
       return this;
-    } 
+    }
+
+    public JsTask RemoveLayerJs( TileLayer layer )
+    {
+      var js = $"{JsName}.removeLayer({layer.JsName});";
+      return new JsTask( js, () => Browser.EvaluateScriptAsync( js) );
+    }
+
+
+    public async Task<Map> RemoveLayerJsd( TileLayer layer )
+    {
+      await RemoveLayerJs( layer ).Run();
+      return this;
+    }
 
     public string DivId { get; }
+  }  
+
+  public class JsTask
+  {
+    public string Js { get; }
+
+    public JavascriptResponse Response { get; set; }
+    private readonly Func<Task<JavascriptResponse>> _runJs;
+
+    public JsTask( string js, Func<Task<JavascriptResponse>> response )
+    {
+      Js = js;
+      _runJs = response;
+    }
+
+    public async Task<JavascriptResponse> Run()
+    {
+      return Response = await _runJs.Invoke();
+    }
   }
 }
